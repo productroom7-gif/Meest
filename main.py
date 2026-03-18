0750dc53ad0c66b87cdff7058221ce5a6906572c
productroom7-gif <productroom7@gmail.com>
2026-03-18 14:55:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привіт! Бот Meest запущений через GitHub.")
+ bot.reply_to(message, "Привіт! Бот Meest запущений через GitHub.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
